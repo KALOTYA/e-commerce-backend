02336c58986911e29c9db7f5f922dd08f916b23e
Ajay Kaloty <kalotya99@gmail.com>
2024-04-28 14:27:51 -0700

    Added get by id route for tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -20,6 +20,19 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagInfoById = await Tag.findByPk(req.params.id, {
+      include: { model: Product },
+    });
+    if (tagInfoById) {
+      res.status(200).json(tagInfoById);
+    } else {
+      res.status(404).json({ message: "Incorrect Id!"});
+      return;
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', async (req, res) => {
