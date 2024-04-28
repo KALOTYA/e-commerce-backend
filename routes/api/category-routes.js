eadcf137af6b2785f3570a0394c2b42ff0310b96
Ajay Kaloty <kalotya99@gmail.com>
2024-04-28 12:40:17 -0700

    Added get id route for category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -16,9 +16,21 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const catDataById = await Category.findByPk(req.params.id, {
+      include: { model: Product },
+    });
+    if (catDataById) {
+      res.status(200).json(catDataById);
+    } else {
+      res.status(404).json({ message: "Incorrect Id!" });
+    } 
+  } catch (err) {
+    res.status(500).json(err);
+  }  
 });
 
 router.post('/', (req, res) => {
